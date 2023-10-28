63c43f906ffaba35c63468270fbedda4dccef1b5
Geoffrey Cleaves <gcleaves@gcleaves.com>
2023-10-28 15:59:07 +0200

    update, insert, delete

--- src/api.controller.ts
+++ src/api.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Param, Req } from '@nestjs/common';
+import { Controller, Get, Param, Req, Patch, Body, Put, Delete } from '@nestjs/common';
 import { Request } from 'express';
 import { ApiService } from './api.service';
 import { ConfigService } from '@nestjs/config';
+import {SheetInsertDto, SheetQueryDto, SheetUpdateDto} from "./dto/sheet-update.dto";
 
 @Controller('/api')
 export class ApiController {
@@ -27,12 +28,33 @@ export class ApiController {
   }
 
   @Get(':sheetId/search_or')
-  search_or(@Param() params: any, @Req() request: Request): any {
+  search_or(@Param() params: any, @Req() request: Request): Record<string, any> {
     const sheet: string = request.query._sheet as string;
 
     const predicates = {...request.query};
     delete predicates._sheet;
 
     return this.apiService.search(params.sheetId, sheet, predicates, 'or');
+  }
+
+  @Patch(':sheetId')
+  update(@Param() params: any, @Req() request: Request, @Body() body: SheetUpdateDto): any {
+    const sheetName: string = request.query._sheet as string;
+    const query = body.query;
+    const update = body.update;
+
+    return this.apiService.update(params.sheetId, sheetName, query, update);
+  }
+
+  @Put(':sheetId')
+  insert(@Param() params: any, @Req() request: Request, @Body() body: SheetInsertDto): any {
+    const sheetName: string = request.query._sheet as string;
+    return this.apiService.insert(params.sheetId, sheetName, body.insert);
+  }
+
+  @Delete(':sheetId')
+  delete(@Param() params: any, @Req() request: Request, @Body() query: SheetQueryDto): any {
+    const sheetName: string = request.query._sheet as string;
+    return this.apiService.delete(params.sheetId, sheetName, query);
   }
 }
