e0049886168186865656e7a0947002d3d8c47899
Geoffrey Cleaves <gcleaves@redpoints.com>
2024-09-26 21:51:00 +0200

    use bullmq to insert 1 at a time

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
-import {NestFactory} from '@nestjs/core';
-import {AppModule} from './app.module';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
 import * as session from 'express-session';
-import {ConfigService} from "@nestjs/config";
+import { ConfigService } from "@nestjs/config";
 import RedisStore from "connect-redis"
 import Redis from 'ioredis';
+import { Worker, Job } from 'bullmq';
+import { ApiService } from './api.service';
+
 const redis = new Redis({
     host: process.env.REDIS_HOST as any || 'localhost',
     port: process.env.REDIS_PORT as any || 6379,
@@ -17,6 +20,8 @@ const redisStore = new RedisStore({
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule, { cors: true });
+    const apiService = app.get(ApiService);
+
     //console.log('SESSION_SECRET', app.get(ConfigService).get('SESSION_SECRET'));
     app.use(
         session({
@@ -26,6 +31,24 @@ async function bootstrap() {
             saveUninitialized: false,
         }),
     );
+
+
+    const worker = new Worker('insert', async (job: Job) => {
+        // Optionally sending an object as progress
+        // Do something with job
+        console.log('job', job.data);
+
+        const sheet = await apiService.getSheet(job.data.uid, job.data.sheetName);
+        const added = await sheet.addRows(job.data.insert);
+        
+        return added.map(a=>a.toObject());
+    }, { 
+        connection: {
+            host: process.env.REDIS_HOST as any || 'localhost',
+            port: process.env.REDIS_PORT as any || 6379,
+            db: process.env.REDIS_DB as any || 1
+        }
+    });
 
     await app.listen(3000);
 }
